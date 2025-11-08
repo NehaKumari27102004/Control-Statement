69f030c17eecf6e51f076c06d099200fc382c0e2
Neha <161303798+NehaKumari27102004@users.noreply.github.com>
2025-11-08 12:19:46 +0530

    Add files via upload

---
+++ CalculatorSwitch.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+
+public class CalculatorSwitch {
+
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Welcome to the Your Own Calculator");
+        System.out.println("Please enter the first number:");
+        int num1=sc.nextInt();
+        System.out.println("Please enter the second number:");
+        int num2=sc.nextInt();
+        System.out.println("Now, enter the operation:");
+        String operation = sc.next();
+
+        int result = switch (operation){
+
+            case "+" -> num1 +num2;
+            case "-" -> num1-num2;
+            case "*" -> num1 * num2;
+            case "/" -> num1 /num2;
+            default -> -1;
+        };
+        System.out.println(result);
+
+
+    }
+}
